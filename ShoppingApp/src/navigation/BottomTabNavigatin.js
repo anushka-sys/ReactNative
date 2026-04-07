1c8dc0e87a126c8f4162ba00aa3d5ab498ebeb62
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-04-07 18:14:50 +0530

    Created BottomTab

---
+++ ShoppingApp/src/navigation/BottomTabNavigatin.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import HomePage from '../screens/HomePage';
+import CartScreen from '../screens/CartScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  const getTabBarIcon = (routeName, focused, color, size) => {
+    let iconName;
+
+    if (routeName === 'Home') {
+      iconName = focused ? 'home' : 'home-outline';
+    } else if (routeName === 'Cart') {
+      iconName = focused ? 'cart' : 'cart-outline';
+    } else if (routeName === 'Profile') {
+      iconName = focused ? 'person' : 'person-outline';
+    }
+
+    return <Icon name={iconName || 'help-circle'} size={size} color={color} />;
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) =>
+          getTabBarIcon(route.name, focused, color, size),
+        tabBarActiveTintColor: '#F83758',
+        tabBarInactiveTintColor: '#000000',
+        tabBarStyle: {
+          paddingBottom: 5,
+          height: 50,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomePage} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
