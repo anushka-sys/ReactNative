2e2d8a511c4e7a633433f9bad96d616bbd1a902c
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-04-17 11:26:03 +0530

    updated files

--- ShoppingApp/src/navigation/AppNavigator.js
+++ ShoppingApp/src/navigation/AppNavigator.js
@@ -1,18 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  CartProvider  from '../context/Context';
+
 import LoginScreen from '../screens/LoginScreen';
 import SignUp from '../screens/SignUp';
 import ForgetPassword from '../screens/ForgetPassword';
 import GetStartedScreen from '../screens/GetStartedScreen';
 import TabNavigator from './TabNavigator';
 import ProductDetails from '../screens/ProductDetails'
+// import DrawerNavigator from './DrawerNavigator'
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <CartProvider>
+   
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
@@ -25,7 +26,7 @@ const AppNavigator = () => {
         <Stack.Screen name="ProductDetails" component={ProductDetails} />
       </Stack.Navigator>
     </NavigationContainer>
-    </CartProvider>
+    
   );
 };
 
---
+++ ShoppingApp/src/navigation/DrawerNavigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import TabNavigator from './TabNavigator';
+import CustomDrawerContent from './CustomDrawerContent'; // we'll create this
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerPosition: 'left', 
+      }}
+    >
+      <Drawer.Screen name="Tabs" component={TabNavigator} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
