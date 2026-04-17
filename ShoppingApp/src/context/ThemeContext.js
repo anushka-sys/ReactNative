c2ed20e1adf6b7d2c07f1922bc61f33a37b2decc
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-04-17 11:27:01 +0530

    update

---
+++ ShoppingApp/src/context/ThemeContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+const lightTheme = {
+  backgroundPrimary: '#ffffff',
+  backgroundMuted: '#f5f5f5',
+  textPrimary: '#000000',
+};
+
+const darkTheme = {
+  backgroundPrimary: '#121212',
+  backgroundMuted: '#1e1e1e',
+  textPrimary: '#ffffff',
+};
+
+const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => setIsDark(prev => !prev);
+  const theme = isDark ? darkTheme : lightTheme;
+
+  return (
+    <ThemeContext.Provider value=
+    {{ 
+      theme, 
+      toggleTheme, 
+      isDark 
+      }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
