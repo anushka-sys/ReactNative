7831cc38899146664df3ffe2ba07bf2684509bd5
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-04-22 11:04:57 +0530

    added custom drawer navigator

--- ShoppingApp/src/navigation/DrawerNavigator.js
+++ ShoppingApp/src/navigation/DrawerNavigator.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import TabNavigator from './TabNavigator';
-// import CustomDrawerContent from './CustomDrawerContent';
+// import CustomDrawer from './CustomDrawer';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
     <Drawer.Navigator
-      // drawerContent={(props) => <CustomDrawerContent {...props} />}
+      // drawerContent={(props) => <CustomDrawer {...props} />}
       screenOptions={{
         headerShown: false,
         drawerPosition: 'left',
@@ -20,3 +20,10 @@ const DrawerNavigator = () => {
 };
 
 export default DrawerNavigator;
+
+
+// console.js:668 Found screens with the same name nested inside one another. Check:
+
+// MainTabs > Home, MainTabs > Home > Home
+
+// This can cause confusing behavior during navigation. Consider using unique names for each screen instead.
