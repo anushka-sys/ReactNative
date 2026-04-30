a498ca0cbe66ae2a114f2667d9c1befaf350cfcf
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-04-30 08:38:51 +0530

    updated files

--- ShoppingApp/src/components/SearchBar.js
+++ ShoppingApp/src/components/SearchBar.js
@@ -7,7 +7,7 @@ import {
   NativeModules,
   DeviceEventEmitter,
   PermissionsAndroid,
-  Platform
+  Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { ThemeContext } from '../context/ThemeContext';
